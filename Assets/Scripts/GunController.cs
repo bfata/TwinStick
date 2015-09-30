1226b3a214ce360884edd2590d3b2ae9903ef9b0
Bobby Fata <bobbyfata@gmail.com>
2015-09-30 15:40:39 -0400

    Gun and projectile set up

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -1,24 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GunController : MonoBehaviour {
+public class GunController : MonoBehaviour
+{
 
 	public Transform weaponHold;
 	public Gun startingGun;
 	Gun equippedGun;
 
-	void Start () {
+	void Start ()
+	{
 		if (startingGun != null) {
-			EquipGun(startingGun);
+			EquipGun (startingGun);
 		}
 	}
 		
-	public void EquipGun (Gun gunToEquip) {
-		if (equippedGun != null){
-			Destroy(equippedGun.gameObject);
+	public void EquipGun (Gun gunToEquip)
+	{
+		if (equippedGun != null) {
+			Destroy (equippedGun.gameObject);
 		}
 		equippedGun = Instantiate (gunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
 		equippedGun.transform.parent = weaponHold;
+	}
+
+	public void Shoot ()
+	{
+		if (equippedGun != null) {
+			equippedGun.Shoot ();
+		}
 	}
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,32 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (PlayerController))]
-public class Player : MonoBehaviour {
+[RequireComponent (typeof(PlayerController))]
+[RequireComponent (typeof(GunController))]
+
+public class Player : MonoBehaviour
+{
 
 	public float moveSpeed = 5;
 
 	Camera viewCamera;
 	PlayerController controller;
+	GunController gunController;
 
-	void Start () {
+	void Start ()
+	{
 		controller = GetComponent<PlayerController> ();
+		gunController = GetComponent<GunController> ();
 		viewCamera = Camera.main;
 	}
 	
-	void Update () {
-		Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+	void Update ()
+	{
+		// Movement input
+		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
 		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
 		controller.Move (moveVelocity);
 
+		// Look input
 		Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
-		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
 		float rayDistance;
 
-		if (groundPlane.Raycast(ray,out rayDistance)) {
+		if (groundPlane.Raycast (ray, out rayDistance)) {
 			Vector3 point = ray.GetPoint (rayDistance);
 			//Debug.DrawLine(ray.origin,point,Color.red);
-			controller.LookAt(point);
+			controller.LookAt (point);
+		}
+
+		// Weapon input
+		if (Input.GetMouseButton (0)) {
+			gunController.Shoot ();
 		
 		}
 	}
