be3c07a5f55a6c123b552bad5af706cab54b9799
Bobby Fata <bobbyfata@gmail.com>
2015-09-30 18:02:06 -0400

    Added enemy, navmesh and projectile hit detection

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,17 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour
+{
 
+	public LayerMask collisionMask;
 	float speed = 10;
 
-	public void SetSpeed (float newSpeed){
+	public void SetSpeed (float newSpeed)
+	{
 		speed = newSpeed;
 
 	}
 
-	void Update () {
-		transform.Translate(Vector3.forward * Time.deltaTime * speed);
+	void Update ()
+	{
+		float moveDistance = speed * Time.deltaTime;
+		CheckCollisions (moveDistance);
+		transform.Translate (Vector3.forward * moveDistance);
+	}
+
+	void CheckCollisions (float moveDistance)
+	{
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide)) {
+			OnHitObject (hit);
+		}
+	}
 	
+	void OnHitObject (RaycastHit hit)
+	{
+		print (hit.collider.gameObject.name);
+		GameObject.Destroy (gameObject);
 	}
 }
