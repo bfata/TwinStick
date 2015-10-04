a87f5511621521f9ea95eca50a75a926dc138353
Bobby Fata <bobbyfata@gmail.com>
2015-10-04 12:49:21 -0400

    Added enemy attack

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,41 @@ using System.Collections;
 [RequireComponent (typeof(NavMeshAgent))]
 public class Enemy : LivingEntity
 {
+	public enum State
+	{
+		Idle,
+		Chasing,
+		Attacking
+	}
+	;
+
+	State currentState;
 
 	NavMeshAgent pathfinder;
 	Transform target;
+	Material skinMaterial;
+
+	Color originalColor;
+
+	float attackDistanceThreshold = .5f;
+	float timeBetweenAttacks = 1;
+
+	float nextAttackTime;
+	float myCollisionRadius;
+	float targetCollisionRadius;
 
 	protected override void Start ()
 	{
 		base.Start ();
 		pathfinder = GetComponent<NavMeshAgent> ();
+		skinMaterial = GetComponent<Renderer> ().material;
+		originalColor = skinMaterial.color;
+
+		currentState = State.Chasing;
 		target = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		myCollisionRadius = GetComponent<CapsuleCollider> ().radius;
+		targetCollisionRadius = target.GetComponent<CapsuleCollider> ().radius;
 
 		StartCoroutine (UpdatePath ());
 	
@@ -21,6 +47,39 @@ public class Enemy : LivingEntity
 	// Update is called once per frame
 	void Update ()
 	{
+		if (Time.time > nextAttackTime) {
+			float sqrDstToTarget = (target.position - transform.position).sqrMagnitude;
+			if (sqrDstToTarget < Mathf.Pow (attackDistanceThreshold + myCollisionRadius + targetCollisionRadius, 2)) {
+				nextAttackTime = Time.time + timeBetweenAttacks;
+				StartCoroutine (Attack ());
+			}
+		}
+	}
+
+	IEnumerator Attack ()
+	{
+		currentState = State.Attacking;
+
+		pathfinder.enabled = false;
+		Vector3 originalPosition = transform.position;
+		Vector3 dirToTarget = (target.position - transform.position).normalized;
+		Vector3 attackPosition = target.position - dirToTarget * (myCollisionRadius);
+
+		float attackSpeed = 3;
+		float percent = 0;
+
+		skinMaterial.color = Color.red;
+		while (percent <= 1) {
+			percent += Time.deltaTime * attackSpeed;
+			float interpolation = (-Mathf.Pow (percent, 2) + percent) * 4;
+			transform.position = Vector3.Lerp (originalPosition, attackPosition, interpolation);
+
+			yield return null;
+		}
+
+		skinMaterial.color = originalColor;
+		currentState = State.Chasing;
+		pathfinder.enabled = true;
 
 	}
 
@@ -29,9 +88,12 @@ public class Enemy : LivingEntity
 		float refreshRate = .25f;
 
 		while (target != null) {
-			Vector3 targetPosition = new Vector3 (target.position.x, 0, target.position.z);
-			if (!dead) {
-				pathfinder.SetDestination (targetPosition);
+			if (currentState == State.Chasing) {
+				Vector3 dirToTarget = (target.position - transform.position).normalized;
+				Vector3 targetPosition = target.position - dirToTarget * (myCollisionRadius + targetCollisionRadius + attackDistanceThreshold / 2);
+				if (!dead) {
+					pathfinder.SetDestination (targetPosition);
+				}
 			}
 			yield return new WaitForSeconds (refreshRate);
 		}
